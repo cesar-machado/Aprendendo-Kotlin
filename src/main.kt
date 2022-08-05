56d220a97617246a11d5f4499750aa8810a47bdb
cesar-machado <cesarmach19@gmail.com>
2022-08-05 16:54:04 -0300

    Adicionando Classe funcionário

--- src/main.kt
+++ src/main.kt
@@ -1,7 +1,12 @@
 fun main() {
     println("Bem vindo ao Bytebank")
 
-    testaComportamentosConta()
+    val alex = Funcionarios(nome = "Alex", cpf = "333.333.222.11", salario = 1000.0  )
+
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("Salário ${alex.salario}")
+    println("Bonificação ${alex.bonificacao()} ")
 }
 
 
